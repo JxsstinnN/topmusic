8b6172858f352ff935dcbc2fea6bb6d37949f9c1
Justin R <justininfotep@gmail.com>
2024-09-17 15:44:32 -0400

    adding sidemenucomponents and index layout

---
+++ src/lib/data.js
@@ -0,0 +1,63 @@
+import { colors } from "./colors";
+
+export const playlists = [
+  {
+    id: "1",
+    title: "R&B",
+    color: colors.teal,
+    cover:
+      "https://res.cloudinary.com/dp3ppkxo5/image/upload/v1693776174/spotify-astro/playlist_1_yci5uf.jpg",
+    artists: ["Avicii", "Alok"],
+  },
+  {
+    id: "2",
+    title: "Pop",
+    color: colors.green,
+    cover:
+      "https://res.cloudinary.com/dp3ppkxo5/image/upload/v1693776174/spotify-astro/playlist_2_f9ymlx.jpg",
+    artists: ["Tiesto", "Armin Van Buuren"],
+  },
+  {
+    id: "3",
+    title: "Rock",
+    color: colors.rose,
+    cover:
+      "https://res.cloudinary.com/dp3ppkxo5/image/upload/v1693776175/spotify-astro/playlist_3_grshca.jpg",
+    artists: ["Post Malone", "Travis Scott", "21 savage"],
+  },
+  {
+    id: "4",
+    title: "K-Pop",
+    color: colors.red,
+    cover:
+      "https://res.cloudinary.com/dp3ppkxo5/image/upload/v1693776175/spotify-astro/playlist_4_ap5xnb.jpg",
+    artists: ["The Beatles"],
+  },
+  {
+    id: "5",
+    title: "Chill",
+    color: colors.pink,
+    cover:
+      "https://res.cloudinary.com/dp3ppkxo5/image/upload/v1693776175/spotify-astro/playlist_5_erjyb7.jpg",
+    artists: ["Deadmau5"],
+  },
+  {
+    id: "6",
+    title: "Trap",
+    color: colors.gray,
+    cover:
+      "https://res.cloudinary.com/dp3ppkxo5/image/upload/v1693776474/spotify-astro/R-15112137-1586815179-1911_fsyl58.jpg",
+    artists: ["Saint Hilda", "Canada Buffalo"],
+  },
+];
+
+export const sidebarPlaylists = [
+ {
+  id: "1_side",
+  title: "Ranking Global",
+  color: colors.teal,
+  cover:
+    "https://res.cloudinary.com/dp3ppkxo5/image/upload/v1693776174/spotify-astro/playlist_1_yci5uf.jpg",
+ }
+];
+
